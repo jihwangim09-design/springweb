e6f0052c2c44b987b695e08a831306d0d6a8b403
jihwangim09-design <jihwangim09@gmail.com>
2026-08-26 12:39:15 +0900

    0826 1239

--- src/main/java/example/day02/model/dto/BoardDto.java
+++ src/main/java/example/day02/model/dto/BoardDto.java
@@ -1,13 +1,9 @@
 package example.day02.model.dto;
 
 public class BoardDto {
-
-    // DTO: 데이터 이동객체??, 자바는 저장소가 아니다. 즉] 데이터베이스(저장소)가 저장소 
-    // 1. 데이터베이스 표에서 (CRUD) 사용할 자료들을 private 멤버변수로 구성
     private int no;
     private String content;
     private String writer;
-    // 2. 기본생성자, 전체매개변수생성자
     public BoardDto() { }
     public BoardDto(int no, String content, String writer) {
         this.no = no;
