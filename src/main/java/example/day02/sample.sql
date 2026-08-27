fddeacf0dad000dfdcf8e92108a73a6b910525d5
jihwangim09-design <jihwangim09@gmail.com>
2026-08-27 11:23:36 +0900

    0827 1123

--- src/main/java/example/day02/sample.sql
+++ src/main/java/example/day02/sample.sql
@@ -2,8 +2,9 @@ DROP DATABASE IF EXISTS mydb0826;
 CREATE DATABASE mydb0826;
 USE mydb0826;
 CREATE TABLE board( 
-    phone VARCHAR(255) ,
-    count int NOT NULL;
-    constraint PRIMARY KEY( phone ) 
+    no int AUTO_INCREMENT , 
+    content VARCHAR(255) ,
+    writer VARCHAR(30) ,
+    constraint PRIMARY KEY( no ) 
 );
 insert into board( content, writer )values( "안녕하세요", "유재석" ),( "하하", "강호동"); -- 샘플 데이터 2개 
