d0848f18fde848fe421e6ae14e2d08e4910adbcc
jihwangim09-design <jihwangim09@gmail.com>
2026-09-02 11:18:24 +0900

    0902 1118

--- src/main/java/example/day03/RestController2.java
+++ src/main/java/example/day03/RestController2.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,10 +47,41 @@ public class RestController2 {
         System.out.println( map );
         return 8;
     }
-    // 5. http://localhost:8080/day03/task8?name=유재석&age=10
+    // 5. http://localhost:8080/day03/task9?name=유재석&age=10
     @DeleteMapping( "/task9")
     public int task9 ( @ModelAttribute ExamDto examDto ){
         System.out.println( examDto );
         return 9;
     }
+    // 6. http://localhost:8080/day03/task10/유재석/10      ?안씀위에처럼
+    @GetMapping( "/task10/{name}/{age}")
+    public int task10( 
+        @PathVariable(name = "name") String name , 
+        @PathVariable(name = "age") int age ){
+        System.out.println( name ); System.out.println( age );
+        return 10;
+    }
+    // 7. http://localhost:8080/day03/task11 , { "name" : "유재석" , "age" : 10 }
+    @PostMapping( "/task11") 
+    public int task11( @RequestBody ExamDto examDto ){
+        System.out.println( examDto );
+        return 11;
+    }
+    
 } // class end
+
+
+/* 
+    요청 Request ------->
+                  HTTP
+            <----------- 응답 Response
+    * HTTP content-type : 전송하는 데이터의 타입 뜻
+    쿼리스트링이란?: ?(물음표)뒤부터 URL?매개변수명=값&매개변수명=값 
+        - GET / POST / PUT / DELETE 모두 사용 가능
+    경로구분자이란? URL/값1/값2
+    1. @RequestParm : 쿼리스트링 또는 content-type:'application/x-www-form-urlencoded'(기본값) 	주 용도:단순 값 몇 개
+    2. @ModelAttribute(생략가능) : + DTO 매핑  주 용도:값이 많을 때 DTO 객체 하나로 묶어서 받음
+    3. @PathVariable : 경로구분자  주 용도:특정 자원 지목
+    4. @RequestBody : content-type:'application/json'  주 용도:JSON 통신(API) 프론트엔드(JS)에서 fetch/axios로 JSON 보낼 때 사용
+        -> POST / PUT 메소드만 body(본문-경로상의값노출이없다) 지원한다.
+    */
---
+++ src/main/java/example/day04/TestController.java
@@ -0,0 +1,80 @@
+package example.day04;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @PostMapping("")  // http://127.0.0.1:8080/test
+    public boolean testWrite(@RequestBody TestDto testDto){
+        System.out.println("TestController.testWrite()");
+        return true;
+    }
+
+    @GetMapping("") // http://127.0.0.1:8080/test
+    public ArrayList<TestDto> testPrint(){
+        System.out.println("TestController.testPrint()");
+        ArrayList<TestDto> result = new ArrayList<>();
+        result.add(new TestDto(1, "내용1", "작성자1"));
+        result.add(new TestDto(2, "내용2", "작성자2"));
+        return result;
+    }
+
+    @GetMapping("/detail")  // http://127.0.0.1:8080/test/detail?no=1
+    public TestDto tsetDetail(@RequestParam(name="no") int no){
+        System.out.println("TestController.tsetDetail()");
+        return new TestDto(1, "내용1", "작성자1");
+    }
+
+    @PutMapping("") // http://127.0.0.1:8080/test
+    public boolean testUpdate(@RequestBody TestDto testDto){
+        System.out.println("TestController.testUpdate()");
+        return true;
+    }
+
+    @DeleteMapping("/{no}")  // http://127.0.0.1:8080/test/1
+    public boolean testDelete(@PathVariable(name="no") int no){
+        System.out.println("TestController.testDelete()"); 
+        return true;
+    }
+}
+
+@Data// getter, setter, tostring 등
+@NoArgsConstructor
+@AllArgsConstructor
+class TestDto{
+    private Integer no;
+    private String content;
+    private String writer;
+}
+
+/*
+    - int: 기본타입 +-21억
+    - Integer: 참조타입(int 래퍼클래스) + null 저장
+    주의할점: http 파싱하는 경우에 int에는 null 저장 불가능
+    권장!: 기본타입 대신 래퍼타입 사용하자 (int 대신에 Integer)
+
+    byte    → Byte
+    short   → Short
+    int     → Integer
+    long    → Long
+    float   → Float
+    double  → Double
+    char    → Character
+    boolean → Boolean
+*/
