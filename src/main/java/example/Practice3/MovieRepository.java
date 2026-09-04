6548f2e858faae8b680f0924668c20f0af391d6c
jihwangim09-design <jihwangim09@gmail.com>
2026-09-04 11:53:09 +0900

    0904 1153

---
+++ src/main/java/example/Practice3/MovieRepository.java
@@ -0,0 +1,13 @@
+package example.Practice3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+ 	
+@Repository
+public interface MovieRepository extends JpaRepository <MovieEntity,Integer> {
+    
+
+    
+}
