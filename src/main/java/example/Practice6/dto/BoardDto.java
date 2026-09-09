264e9a0b3cf918e6e7e52ae11359efb8ae515b23
jihwangim09-design <jihwangim09@gmail.com>
2026-09-09 15:35:53 +0900

    0909 1535

--- src/main/java/example/Practice6/dto/BoardDto.java
+++ src/main/java/example/Practice6/dto/BoardDto.java
@@ -1,21 +1,5 @@
 package example.Practice6.dto;
 
-import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
-
 public class BoardDto {
-    private Integer boardId;
-    private String author;
-    private String password;
-    private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    @Builder.Default
 }
--- src/main/java/example/day07/AppStart.java
+++ src/main/java/example/day07/AppStart.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class AppStart {
-@SpringBootApplication 
+@SpringBootApplication  
     public static void main(String[] args) {
         SpringApplication.run(AppStart.class);
     }
